d13e20572fe998adae2feabf0850ad8a890639ea
vctrgd <rigaudvictor13@gmail.com>
2025-02-19 10:58:00 +0100

    ajout logique de bouttons pour cycler les filtres

--- projetSonVoiceFilter.ino
+++ projetSonVoiceFilter.ino
@@ -7,21 +7,46 @@ AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
 AudioConnection patchCord0(filters,0,out,0);
 AudioConnection patchCord1(filters,0,out,1);
-
+bool isDown;
+bool isRelease;
+int digitalReadConverted = 0;
 
 void setup() {
+  pinMode(0, INPUT);
   AudioMemory(2);
   audioShield.enable();
   audioShield.volume(0.2);
 }
 
 void loop() {
-  filters.setParamValue("mode",1);
-  Serial.println(filters.getParamValue("mode"));
-  delay(1000);
-  filters.setParamValue("mode",0);
-  Serial.println(filters.getParamValue("mode"));
-  delay(500);
+  int digitalReadValue = digitalRead(0);
+
+  if(digitalReadValue==1 && isDown==false) {
+    isDown=true;
+    digitalReadConverted++;
+    if (digitalReadConverted > 2) {  
+      digitalReadConverted = 0;
+    }
+    Serial.print("State: ");
+    Serial.println(digitalReadConverted); 
+  }
+  else if(digitalReadValue== 0 && isDown==true) {
+    isDown = false;
+  }
+
+  if (digitalReadConverted ==1) {
+    filters.setParamValue("mode",0);
+    Serial.println(filters.getParamValue("mode"));  
+  }
+  
+  else if (digitalReadConverted == 2) {
+    filters.setParamValue("mode",1);
+    Serial.println(filters.getParamValue("mode"));
+  }
+  else if (digitalReadConverted == 3) {
+  }
+
+  delay(50);
 
 }
 
